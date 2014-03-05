2154856c4ddc9e8fe4d5de73da35b8ce96f5ff4f
codeprogress <codeprogress1@gmail.com>
2014-03-04 23:57:44 -0500

    Update cards.py, add action card descriptions

--- cards.py
+++ cards.py
@@ -19,7 +19,7 @@ class Card(object):
         self.action   = action
 
                                 
-#methods to create cards
+#methods to create cards (avoid globals)
 
 #Treasure cards
 def gold():
@@ -42,3 +42,129 @@ def estate():
     return Card("estate",   2, "v", 0, 1)
 
 
+##Special Victory cards
+#def Gardens():
+#    """Worth 1 Victory for every 10 cards in your deck (rounded down)"""
+#    pass
+##Action cards (ordered by price)
+#def Cellar():
+#    """+1 Action
+#    Discard any number of cards.
+#    +1 Card per card discarded
+#    """
+#    pass
+#    
+#def Chapel():
+#    """Trash up to 4 cards from your hand"""
+#    pass
+#    
+#def Moat():
+#    """+2 Cards
+#    When another player plays an Attack card, 
+#    you may reveal this from your hand. 
+#    If you do, you are unaffected by that Attack
+#    """
+#    pass
+#    
+#def Chancellor():
+#    """+$2, You may immediately put your deck into your discard pile"""
+#    pass
+#
+#def Village():
+#    """+1 Card; +2 Actions"""
+#    pass
+#
+#def Woodcutter():
+#    """+1 Buy; +$2"""
+#    pass
+#
+#def Workshop():
+#    """Gain a card costing up to $4"""
+#    pass
+#
+#def Bureaucrat():
+#    """Gain a silver card; put it on top of your deck. 
+#    Each other player reveals a Victory card from his hand and 
+#    puts it on his deck (or reveals a hand with no Victory cards)"""
+#    pass
+#    
+#def Feast():
+#    """Trash this card. Gain a card costing up to $5"""
+#    pass
+#
+#def Militia():
+#    """+$2
+#    Each other player discards down to 3 cards in his hand"""
+#    pass
+#
+#def Moneylender():
+#    """Trash a Copper from your hand. If you do, +$3"""
+#    pass
+#
+#def Remodel():
+#    """Trash a card from your hand. 
+#    Gain a card costing up to $2 more than the trashed card"""
+#    pass
+#    
+#def Smithy():
+#    """+3 Cards"""
+#    pass
+#
+#def Spy():
+#    """+1 Card; +1 Action
+#    Each player (including you) reveals the top card of his deck and 
+#    either discards it or puts it back, your choice"""
+#    pass
+#    
+#def Thief():
+#    """Each other player reveals the top 2 cards of his deck. 
+#    If they revealed any Treasure cards, they trash one of them that you choose. 
+#    You may gain any or all of these trashed cards. 
+#    They discard the other revealed cards"""
+#    pass
+#
+#def ThroneRoom():
+#    """Choose an Action card in your hand. Play it twice"""
+#    pass
+#
+#def CouncilRoom():
+#    """+4 Cards; +1 Buy
+#    Each other player draws a card"""
+#    pass
+#
+#def Festival():
+#    """+2 Actions, +1 Buy; +$2"""
+#    pass
+#
+#def Laboratory():
+#    """+2 Cards; +1 Action"""
+#    pass
+#    
+#def Library():
+#    """Draw until you have 7 cards in hand. 
+#    You may set aside any Action cards drawn this way, as you draw them; 
+#    discard the set aside cards after you finish drawing"""
+#    pass
+#    
+#def Market():
+#    """+1 Card; +1 Action; +1 Buy; +$1"""
+#    pass
+#
+#def Mine():
+#    """Trash a Treasure card from your hand. 
+#    Gain a Treasure card costing up to $3 more; 
+#    put it into your hand"""
+#    pass
+#
+#def Witch():
+#    """+2 Cards
+#    Each other player gains a Curse card"""
+#    pass
+#    
+#def Adventurer():
+#    """Reveal cards from your deck until you reveal 2 Treasure cards. 
+#    Put those Treasure cards in your hand and discard the other revealed cards
+#    """
+#    pass
+#
+#
