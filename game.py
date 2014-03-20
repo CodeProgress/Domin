5403aec80d3d8fb9bb4f06ffbc1ddac07a88b307
codeprogress <codeprogress1@gmail.com>
2014-03-20 08:53:22 -0400

    Update game.py

--- game.py
+++ game.py
@@ -36,24 +36,28 @@ class Game(object):
     def buy_phase(self, player):
         self.tally_coins(player)
         while player.numBuys > 0:
-            availCards = self.gameDeck.get_all_cards_below(player.numCoins)# key:price, value:card
+            #availCards = []
+            #for i in self.gameDeck.get_all_cards_below(player.numCoins):# key:price, value:card
+            #    if i in self.gameDeck.deck:
+            #        availCards.append(i)
             print "It's your turn {}!".format(player.name)
-            print "You have {} coins".format(player.numCoins)
-            print "You can buy: {}".format(availCards)
-            cardName = raw_input("Which card would you like to buy? ")
-            cardName.lower()
-            try:
-                self.gameDeck.rem_one_card(cardName)
-            except:
+            #print "You have {} coins".format(player.numCoins)
+            #print "You can buy: {}".format(availCards)
+            #cardName = raw_input("Which card would you like to buy? ")
+            #cardName.lower()
+            
+            cardName = self.strat_only_buy_prov(player)
+
+            if cardName in self.gameDeck.deck: 
+                try:
+                    card = self.gameDeck.get_card(cardName)
+                    player.buy(card)
+                    self.gameDeck.rem_one_card(cardName)
+                    print "{} successfully purchased".format(cardName)
+                except AssertionError:
+                    print "Insufficient funds / no more buys"
+            else:
                 print "{} is not in the deck".format(cardName)
-            
-            try:
-                card = self.gameDeck.get_card(cardName)
-                player.buy(card)
-                print "{} successfully purchased".format(cardName)
-            except AssertionError:
-                print "Insufficient funds / no more buys"
-            
     
     def cleanup_phase(self, player):
         player.cleanup()
@@ -73,18 +77,34 @@ class Game(object):
             player = self.players[self.playerToMoveNext]
             self.turn(player)
             self.incr_player_to_move_next()
-        self.verify_scores()
-        self.find_winner
+        self.calc_scores()
+        print self.find_winner()
         
-    def verify_scores(self):
+    def calc_scores(self):
         for player in self.players:
-            check = sum(card.pointVal for card in player.allCards.elements())
-            assert check == player.numPoints
+            player.numPoints = sum([self.gameDeck.get_card(card).pointVal for card in player.allCards.elements()])
     
     def find_winner(self):
         scores = sorted((player.numPoints, player.name) for player in self.players)
         return scores[-1]
     
+    def strat_only_buy_prov(self, player):
+        """strategy where player only buys provinces if numCoins >= 8
+        otherwise, player buys the most valuable treasure card.
+        returns the card to buy (as String) if numCoins > 0, else returns None
+        """
+        coins = player.numCoins
+        if coins >= 8:
+            return "province"
+        if coins >= 6:
+            return "gold"
+        if coins >= 3:
+            return "silver"
+        if coins >= 1:
+            return "copper"
+        return None
+    
+        
 
 domin = Game(2)
 #domin.players[0].numCoins = 11
