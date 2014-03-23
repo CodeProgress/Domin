4d1314d23870d7b7459de4df239b650ce56f28fa
codeprogress <codeprogress1@gmail.com>
2014-03-22 23:59:35 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
 import cards
 import player
 import cProfile
+import strategies
 
 class Game(object):
-    def __init__(self, numPlayers):
+    def __init__(self, numPlayers, AIPlayers = []):
+        """numPlayers: int
+        AIPlayers:  list of tuples: (name, strategy)
+            name: string
+            strategy: tuple: (buyStrategy, actionStrategy)
+        """
         self.numPlayers = numPlayers
 
         startingDeck = ["estate"]*3 + ["copper"]*7
@@ -11,7 +17,10 @@ class Game(object):
         self.players = []
         for i in range(numPlayers):
             self.players.append(player.Player("Player" + str(i), startingDeck))
-
+        
+        for i in AIPlayers:
+            self.players.append(player.AIPlayer(i[0], i[1]))       
+        
         self.gameDeck = cards.Deck()
         
         self.playerToMoveNext = 0
@@ -37,17 +46,19 @@ class Game(object):
     def buy_phase(self, player, verbose = False):
         self.tally_coins(player)
         while player.numBuys > 0:
-            #availCards = []
-            #for i in self.gameDeck.get_all_cards_below(player.numCoins):# key:price, value:card
-            #    if i in self.gameDeck.deck:
-            #        availCards.append(i)
-            if verbose: print "It's your turn {}!".format(player.name)
-            #print "You have {} coins".format(player.numCoins)
-            #print "You can buy: {}".format(availCards)
-            #cardName = raw_input("Which card would you like to buy? ")
-            #cardName.lower()
             
-            cardName = self.strat_only_buy_prov(player)
+            if player.isAI:
+                cardName = player.buyStrategy
+            else:
+                availCards = []
+                for i in self.gameDeck.get_all_cards_below(player.numCoins):# key:price, value:card
+                    if i in self.gameDeck.deck:
+                        availCards.append(i)
+                if verbose: print "It's your turn {}!".format(player.name)
+                print "You have {} coins".format(player.numCoins)
+                print "You can buy: {}".format(availCards)
+                cardName = raw_input("Which card would you like to buy? ")
+                cardName.lower()
 
             if cardName in self.gameDeck.deck: 
                 try:
@@ -93,47 +104,6 @@ class Game(object):
     def find_winner(self):
         scores = sorted((player.numPoints, player.name) for player in self.players)
         return scores
-    
-    
-    
-    ##### 
-    #Strategies for AI players.
-    #Try working this into the player class
-    #ex:  player.strategy = function (default = None)
-    #then in game, call the function
-    #if strategy, return what strategy dictates, otherwise accept user input
-    def strat_only_buy_prov(self, player):
-        """strategy where player only buys provinces if numCoins >= 8
-        otherwise, player buys the most valuable treasure card.
-        returns the card to buy (as String) if numCoins > 0, else returns None
-        """
-        coins = player.numCoins
-        if coins >= 8:
-            return "province"
-        if coins >= 6:
-            return "gold"
-        if coins >= 3:
-            return "silver"
-        if coins >= 1:
-            return "copper"
-        return None
-    
-    def strat_buy_best(self, player):
-        coins = player.numCoins
-        if coins >= 8:
-            return "province"
-        if coins >= 6:
-            return "gold"
-        if coins == 5:
-            return "dutchy"
-        if coins >= 3:
-            return "silver"
-        if coins == 2:
-            return "estate"
-        if coins == 1:
-            return "copper"
-        return None
-        
         
 
 
