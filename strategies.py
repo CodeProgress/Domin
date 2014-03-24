ed584983c3cf62bfa7e352e98711672310f0697b
codeprogress <codeprogress1@gmail.com>
2014-03-23 20:16:39 -0400

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -5,7 +5,7 @@
 #then in game, call the function
 #if strategy, return what strategy dictates, otherwise accept user input
 
-def buy_only_prov(self, numCoins):
+def buy_only_prov(numCoins):
     """strategy where player only buys provinces if numCoins >= 8
     otherwise, player buys the most valuable treasure card.
     returns the card to buy (as String) if numCoins > 0, else returns None
@@ -21,7 +21,7 @@ def buy_only_prov(self, numCoins):
         return "copper"
     return None
 
-def buy_best_avail(self, numCoins):
+def buy_best_avail(numCoins):
 
     if numCoins >= 8:
         return "province"
@@ -37,7 +37,7 @@ def buy_best_avail(self, numCoins):
         return "copper"
     return None
 
-def action_trivial(self, actions):
+def action_trivial(actions):
     """returns the action to play
     actions: list of action functions
     """
